f0ed0db015ca93aea2847c551143e4c055df8720
nicolassaad <nicolassaad1984@gmail.com>
2021-10-24 00:34:14 -0700

    consolidated files and fixed bugs

--- timely-greeting.php
+++ timely-greeting.php
@@ -1,22 +1,20 @@
 <?php
 //------ PHP TIMELY GREETING -------//
-//---------- Version 1.8 ----------//
+//---------- Version 2.0 ----------//
 //-------- BY NICOLAS SAAD ---------//
 //-------- nicolassaad.com ---------//
 
 //--DESCRIPTION:------------------------------------------------------------------------------//
 /* A PHP plugin that displays different greeting messages based on the user's local time.
    This plugin detect a user's time zone using jQuery. First, the current user's timezone offset
-   (in minutes) is retrieved and stored in a variable. Next, PHP collects this variable using the
+   (in minutes) is retrieved and stored in a variable. Next, PHP collects this variable using
    $POST. Finally, the timezone is converted into a readable timezone in text-form that the code
    can then read. */
-
 //--HOW TO USE:-------------------------------------------------------------------------------//
-/* 1. Download both plugin files and store them in your project.
-   2. Include the file 'timely-greeting.php' at the top of the page you want to use it in.
+/* 1. Download the plugin file (timely-greeting.php) and store it in your project.
+   2. Make sure to include the file 'timely-greeting.php' at the top of the page you want to use it in.
    Example: <?php include('includes/timely-greeting.php'); ?>
-   3. In the exact spot on your page where you wan the greeting to display, add this line of code:
-
+   3. In the exact spot on your web page where you want the greeting to display, add this line of HTML:
    <span class="timelyGreeting"></span> <!-- greeting outputs inside span tags -->
    */
 //--------------------------------------------------------------------------------------------//
@@ -67,6 +65,66 @@ if( isset($_POST['name']) ){
     } elseif ( ($now >= $eveningStart) && ( $now <= $eveningEnd) ) {
          echo $greetings["evening"];
     }
+exit; // escapes the rest of the timezone-ajax.html page from printing
 }
-include('timezone-ajax.html');
 ?>
+
+<!DOCTYPE html>
+<html>
+<head><meta charset="utf-8"></head>
+  <body>
+    <!-- Begin jQuery -->
+    <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+    <!-- Timezone offset Retrieval jQuery -->
+    <script>
+      var d = new Date();
+      var month = d.getMonth();
+      var day = d.getDate();
+      var dow = d.getDay();
+
+      var timezone_offset_minutes = new Date().getTimezoneOffset();
+      timezone_offset_minutes = timezone_offset_minutes == 0 ? 0 : -timezone_offset_minutes; // if the offset is 0 leave it alone or else negate the offset value (example: 100 becomes -100).
+
+      console.log( "User's local TZ offset in minutes: " + timezone_offset_minutes); // logging the user's timezone offset in min
+
+      // Checks if DST is on and subtracts 60 minutes from the UTC offset to correctly adjust for Daylight Savings
+      if (isDST(day, month, dow)) { // isDST returns "true"
+          timezone_offset_minutes = timezone_offset_minutes - 60;
+          console.log("DST in North America is Currently ON (Started on the 2nd Sun of Mar & will end on the 1st Sun of Nov)");
+      } else { // isDST returns "false"
+          console.log("DST in North America is Currently OFF");
+      }
+
+      // Function that checks if DST in North America is on or off. Returns true for on and false for off.
+      function isDST(day, month, dow) {
+        //January, february, and december are out.
+        if (month < 2 || month > 10) { return false; }
+        //April to October are in
+        if (month > 2 && month < 10) { return true; }
+        var previousSunday = day - dow; //Getting the previous sunday day by subtracting the dow from the day of month.
+        //In march, we are DST if our previous sunday was on or after the 8th.
+        if (month == 2) { return previousSunday >= 8; }
+        //In november we must be before the first sunday to be in dst.
+        //That means the previous sunday must be before the 1st.
+        return previousSunday <= 0;
+      }
+
+      function fetchdata(){
+      $.ajax({
+      type: 'post',
+      data: {name: timezone_offset_minutes},
+      datatype: 'text',
+      success: function(data){
+        // Perform operation on the return value
+        $('.timelyGreeting').text(data) /* Using 'exit;' at the end of if( isset($_POST['name']) in timely-greeting.php*/
+       }
+     });
+    }
+
+    $(document).ready(function(){
+     fetchdata(); // executing fetchdata() first so the greeting displays when the page loads
+     setInterval(fetchdata, 30000); // interval set to update greeting every 30 seconds. Greeting is updated live on the page without reloading
+    });
+    </script>
+  </body>
+</html>
